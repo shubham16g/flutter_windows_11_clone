7fe860ac79a1821b20cf77aa4a4ee00af22ffaf6
Shubham Gupta <shubham.developer16@gmail.com>
2024-09-25 01:06:14 +0530

    Animation performance improved. Fullscreen blurred wallpaper fix.

--- lib/os/draggable_window/draggable_app.dart
+++ lib/os/draggable_window/draggable_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_windows_11_clone/utils/ui_utils.dart';
+import 'package:flutter_windows_11_clone/widgets/value_animated_builder.dart';
 import 'package:provider/provider.dart';
 
 import 'app_controller.dart';
@@ -24,67 +25,62 @@ class DraggableApp extends StatelessWidget {
     final height = c.isFullScreen ? context.screenSize.height : c.height;
     final double top = c.isFullScreen ? 0 : c.top;
     final double left = c.isFullScreen ? 0 : c.left;
-    return AnimatedPositioned(
+    return ValueAnimatedBuilder(
         duration: Duration(milliseconds: c.isFullScreenAnim ? 300 : 0),
         curve: Curves.easeOutCubic,
-        top: c.isMinimized
-            ? context.screenSize.height
-            : top,
-        left: c.isMinimized
-            ? (context.screenSize.width / 2 - 20)
-            : left,
-        child: MouseRegion(
-          onEnter: c.onHoverEnter,
-          onHover: c.onHover,
-          onExit: c.onHoverExit,
-          child: GestureDetector(
-            onTapDown: onTapDown == null
-                ? null
-                : (details) {
-                    onTapDown!();
-                  },
-            onPanDown: (details) {
-              onTapDown?.call();
-              c.panStart(details);
-            },
-            onPanEnd: (details) {
-              c.panEnd(context.screenSize);
-            },
-            onPanCancel: () {
-              c.panEnd(context.screenSize);
-            },
-            onPanUpdate: (details) {
-              // if drag from sides, change width or height
-              c.onPanUpdate(details.delta.dx, details.delta.dy,
-                  details.localPosition.dx, details.localPosition.dy);
-            },
-            child: Builder(
-              builder: (context) {
-                return AnimatedOpacity(
-                  duration: Duration(milliseconds: c.isOpenClose ? 200 : 0),
-                  opacity: c.isOpenClose ? 0 : 1,
-                  curve: Curves.easeIn,
-                  child: AnimatedScale(
-                    duration: Duration(
-                        milliseconds: c.isFullScreenAnim
-                            ? 300
-                            : (c.isOpenCloseAnim ? 200 : 0)),
-                    curve: c.isOpenClose ? Curves.easeIn : Curves.easeOutCubic,
-                    scale: c.isMinimized ? 0 : (c.isOpenClose ? 0.9 : 1),
-                    alignment:
-                        c.isFullScreenAnim ? Alignment.topLeft : Alignment.center,
-                    child: AnimatedContainer(
-                      duration:
-                          Duration(milliseconds: c.isFullScreenAnim ? 300 : 0),
-                      curve: Curves.easeOutCubic,
-                      width: width,
-                      height: height,
-                      child: c.app.builder(
-                          context, Rect.fromLTWH(left, top, width, height)),
-                    ),
+        top: c.isMinimized ? context.screenSize.height : top,
+        left: c.isMinimized ? (context.screenSize.width / 2 - 20) : left,
+        width: width,
+        height: height,
+        builder: (context, top, left, width, height) => Positioned(
+          top: top,
+          left: left,
+          child: MouseRegion(
+            onEnter: c.onHoverEnter,
+            onHover: c.onHover,
+            onExit: c.onHoverExit,
+            child: GestureDetector(
+              onTapDown: onTapDown == null
+                  ? null
+                  : (details) {
+                      onTapDown!();
+                    },
+              onPanDown: (details) {
+                onTapDown?.call();
+                c.panStart(details);
+              },
+              onPanEnd: (details) {
+                c.panEnd(context.screenSize);
+              },
+              onPanCancel: () {
+                c.panEnd(context.screenSize);
+              },
+              onPanUpdate: (details) {
+                // if drag from sides, change width or height
+                c.onPanUpdate(details.delta.dx, details.delta.dy,
+                    details.localPosition.dx, details.localPosition.dy);
+              },
+              child: AnimatedOpacity(
+                duration: Duration(milliseconds: c.isOpenClose ? 200 : 0),
+                opacity: c.isOpenClose ? 0 : 1,
+                curve: Curves.easeIn,
+                child: AnimatedScale(
+                  duration: Duration(
+                      milliseconds: c.isFullScreenAnim
+                          ? 300
+                          : (c.isOpenCloseAnim ? 200 : 0)),
+                  curve: c.isOpenClose ? Curves.easeIn : Curves.easeOutCubic,
+                  scale: c.isMinimized ? 0 : (c.isOpenClose ? 0.9 : 1),
+                  alignment:
+                      c.isFullScreenAnim ? Alignment.topLeft : Alignment.center,
+                  child: SizedBox(
+                    width: width,
+                    height: height,
+                    child: c.app.builder(
+                        context, Rect.fromLTWH(left, top, width, height)),
                   ),
-                );
-              }
+                ),
+              ),
             ),
           ),
         ));
---
+++ lib/widgets/value_animated_builder.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class ValueAnimatedBuilder extends ImplicitlyAnimatedWidget {
+  /// Creates a container that animates its parameters implicitly.
+  ValueAnimatedBuilder({
+    super.key,
+    required this.top,
+    required this.left,
+    required this.width,
+    required this.height,
+    required this.builder,
+    super.curve,
+    required super.duration,
+    super.onEnd,
+  });
+
+  final double top;
+  final double left;
+  final double width;
+  final double height;
+  final Widget Function(BuildContext context, double top, double left,
+      double width, double height) builder;
+
+  @override
+  AnimatedWidgetBaseState<ValueAnimatedBuilder> createState() =>
+      _ValueAnimatedBuilderState();
+}
+
+class _ValueAnimatedBuilderState
+    extends AnimatedWidgetBaseState<ValueAnimatedBuilder> {
+  Tween<double>? _top;
+  Tween<double>? _left;
+  Tween<double>? _width;
+  Tween<double>? _height;
+
+  @override
+  void forEachTween(TweenVisitor<dynamic> visitor) {
+    _top = visitor(_top, widget.top,
+        (dynamic top) => Tween<double>(begin: top as double)) as Tween<double>?;
+    _left = visitor(_left, widget.left,
+            (dynamic left) => Tween<double>(begin: left as double))
+        as Tween<double>?;
+    _width = visitor(_width, widget.width,
+            (dynamic width) => Tween<double>(begin: width as double))
+        as Tween<double>?;
+    _height = visitor(_height, widget.height,
+            (dynamic height) => Tween<double>(begin: height as double))
+        as Tween<double>?;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Animation<double> animation = this.animation;
+    return widget.builder(
+      context,
+      _top!.evaluate(animation),
+      _left!.evaluate(animation),
+      _width!.evaluate(animation),
+      _height!.evaluate(animation),
+    );
+  }
+}
